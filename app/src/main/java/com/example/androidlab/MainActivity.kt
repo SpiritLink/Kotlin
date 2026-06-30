9035ba43f87eb1ad797e0c3f49df6ba146e8debd
yoo jisang <digndign2@gmail.com>
2026-06-30 09:33:18 +0900

    feat: 터치 이벤트에 따른 이미지 전환 기능 구현 및 SDK 업데이트
    • 이미지 전환: flower1과 flower2 리소스를 추가하고, 화면 터치 시(ACTION_DOWN) 이미지가 바뀌는 로직 구현
    • UI 구성: activity_main.xml에 전체 화면 ImageView 적용
    • 환경 설정: androidx.core 호환성을 위해 compileSdk 및 targetSdk를 37로 상향 및 필요한 라이브러리 의존성 추가

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.androidlab
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +12,11 @@ import androidx.core.view.WindowInsetsCompat
 
 // 버전 호환성을 위해 AppCompatActivity 를 상속받는다.
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+    lateinit var button: Button
+    lateinit var editView: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,8 +25,21 @@ class MainActivity : AppCompatActivity() {
         // 두개를 합쳐서 system 영역이라 부른다.
         enableEdgeToEdge()
 
-        // 화면 출력 명령..
+        // 화면 출력 명령.. : inflate(xml 에 명시된 뷰 객체 생성, 메모리에 올리는 작업)
         setContentView(R.layout.activity_main)
+
+        // 필요한 뷰 객체 획득
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.Button)
+        editView = findViewById(R.id.editView)
+
+        // 뷰에 이벤트 등록
+        button.setOnClickListener {
+            // 유저 입력값 획득
+            val data = editView.text.toString()
+            Log.d("kkang", data)
+            textView.text = editView.text.toString()
+        }
 
         // 시스템 영역에서 액티비티 컨텐츠를 보호해서 출력하기 위한 설정..
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
