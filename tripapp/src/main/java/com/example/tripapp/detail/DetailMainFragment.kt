69b05f21387ceb1b908cc2c5ec9b130e4b9e096c
yoo jisang <digndign2@gmail.com>
2026-07-02 11:09:14 +0900

    feat: DetailMainFragment에 RecyclerView 뼈대 및 아이템 레이아웃 구현
    - DetailMainFragment에 리스트 데이터를 보여주기 위한 RecyclerView 뼈대 추가
    - Product 데이터 클래스 및 RecyclerView용 MyHolder(ItemRecyclerviewBinding 적용), MyAdapter 구현
    - 각 리스트 항목의 구분과 여백 조절을 위한 MyDecoration(ItemDecoration) 데코레이터 추가
    - fragment_detail_main.xml에 RecyclerView 배치
    - 개별 항목 디자인을 위한 item_recyclerview.xml 카드뷰 레이아웃 리소스 추가

--- tripapp/src/main/java/com/example/tripapp/detail/DetailMainFragment.kt
+++ tripapp/src/main/java/com/example/tripapp/detail/DetailMainFragment.kt
@@ -1,11 +1,74 @@
 package com.example.tripapp.detail
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tripapp.databinding.FragmentDetailMainBinding
+import com.example.tripapp.databinding.ItemRecyclerviewBinding
+
+// 각 항목 데이터 추상화 ..
+class Product(val photo: Int, val title: String, val desc: String)
+
+// 항목을 구성하기 위한 뷰를 가지는 역할자..
+class MyHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
+
+// View Holder를 이용해 각 항목을 만드는 역할자
+class MyAdapter(val datas: MutableList<Product>) : RecyclerView.Adapter<MyHolder>() {
+    // 항목 갯수 판단위해 자동 호출
+    override fun getItemCount(): Int {
+        return datas.size // 1000 개가 리턴되었다고 하더라도.. onBindViewHolder를 1000 번 호출하지 않는다.
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyHolder {
+        return MyHolder(
+            ItemRecyclerviewBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: MyHolder,
+        position: Int
+    ) {
+        val product = datas.get(position)
+        holder.binding.run {
+            itemTitleView.text = product.title
+            itemDescView.text = product.desc
+            itemImageView.setImageResource(product.photo)
+        }
+    }
+}
+
+class MyDecoration(val context: Context): RecyclerView.ItemDecoration() {
+    // 각 항목을 꾸미기 위해서 호출
+    // outRect - 항목 구성 사각형 정보
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        // 매개변수에 몇번째 항목을 위해서 호출된 것인지 전달이 되지 않아서..
+        val index = parent.getChildAdapterPosition(view) + 1
+        if(index % 3 == 0){
+            outRect.set(10, 10, 10, 60)
+        } else {
+            outRect.set(10, 10, 10, 0)
+        }
+    }
+}
 
 class DetailMainFragment : Fragment() {
 
@@ -15,7 +78,7 @@ class DetailMainFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val binding = FragmentDetailMainBinding.inflate(inflater, container, false)
         return binding.root
     }
